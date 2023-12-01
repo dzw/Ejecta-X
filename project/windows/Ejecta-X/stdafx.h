7c3982ca38a0d2c642a8559c50532e630523b0a3
dzw <dzwwgy@163.com>
2023-12-01 10:26:39 +0800

    fix R6034

--- project/windows/Ejecta-X/stdafx.h
+++ project/windows/Ejecta-X/stdafx.h
@@ -4,6 +4,7 @@
 //
 
 #pragma once
+#pragma comment(linker, "\"/manifestdependency:type='Win32' name='Microsoft.VC80.CRT' version='8.0.50608.0' processorArchitecture='X86' publicKeyToken='1fc8b3b9a1e18e3b' language='*'\"")
 
 #include "targetver.h"
 
